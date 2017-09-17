518cbd90a6540c1680117e848d0527a70aa3d3e3
Anna <apuhtina.mmdigital@gmail.com>
2017-09-17 22:16:30 +0300

    footer

--- resources/views/widgets/footer_list.blade.php
+++ resources/views/widgets/footer_list.blade.php
@@ -1,5 +1,5 @@
 <div class="navigation-footer__column navigation-footer__column_dropdown">
-    <div class="navigation-footer__title js-toggle-active">{{$config['page_title']}}<i class="sprite_main sprite_main-icon__arrow_green_down"></i>
+    <div class="navigation-footer__title js-toggle-active">{{$config['page_title']}}<i class="sprite sprite-arrow-down-min"></i>
     </div>
     <ul>
         @include('blocks.info-additional')
