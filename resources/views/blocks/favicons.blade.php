b11c25918cbd9bb8cff07ed81ba1f7308b8efd43
name <apuhtina.mmdigital@gmail.com>
2017-11-07 13:23:32 +0300

    07.11_favicons

--- resources/views/blocks/favicons.blade.php
+++ resources/views/blocks/favicons.blade.php
@@ -1,4 +1,4 @@
 <link rel="shortcut icon" href="/img/favicon/_Belberg.ico" type="image/x-icon"/>
-{{--<link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png?v=1"/>--}}
-{{--<link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png?v=1"/>--}}
-{{--<link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png?v=1"/>--}}
+<link rel="apple-touch-icon" href="/img/favicon/apple-touch-icon.png?v=1"/>
+<link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-touch-icon-72x72.png?v=1"/>
+<link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-touch-icon-114x114.png?v=1"/>
