d590f52d048fb1a8b44886bc0790252e00f3f806
name <apuhtina.mmdigital@gmail.com>
2017-11-14 14:40:03 +0300

    14.11.17

--- resources/views/order/partials/success.blade.php
+++ resources/views/order/partials/success.blade.php
@@ -5,5 +5,5 @@
     <div class="order-success__number">Номер Вашего заказа<span> № {{ $order_id }}</span>
     </div>
     <div class="order-success__text">Наш менеджер перезвонит Вам на номер в течении нескольких часов (в рабочее время) для согласования времени доставки . Пожалуйста, не отключайте свой телефон
-    </div><a class="btn btn_green order-success__btn" href="/"><span>На главную</span><i class="sprite_main sprite_main-icon-arrow-small-right-dark-green"></i></a>
+    </div><a class="btn btn_green order-success__btn" href="/"><span>На главную</span></a>
 </div>
