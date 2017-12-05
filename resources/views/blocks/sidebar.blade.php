64442ccbcc975910d8c458c0ac7771ea36c1daa1
Anna <apuhtina.mmdigital@gmail.com>
2017-12-06 00:26:00 +0300

    05.12..

--- resources/views/blocks/sidebar.blade.php
+++ resources/views/blocks/sidebar.blade.php
@@ -14,12 +14,14 @@
         </div>
 
         <div class="mobile-sidebar__level-2 sidebar-geo__level-2 js-geo-mobile">
-            <button class="btn btn_more"><i class="sprite sprite-arrow-top-min"></i><span>Вернуться назад</span></button>
+            {{--<button class="btn btn_more"><i class="sprite sprite-arrow-top-min"></i><span>Вернуться назад</span></button>--}}
+            <a class="mobile-sidebar__title2-back" ><span>Город</span> <i class="sprite sprite-arrow-down-blue2-min"></i>
+            </a>
 
             <form class="form-search geo-city__search js-prevent" method="POST">
                 {{ csrf_field() }}
                 <button class="icon-fade" type="submit">
-                    <i class="sprite_main sprite_main-header__search_active normal"></i>
+                    <i class="sprite sprite-header__search_active normal"></i>
                     <i class="sprite sprite-search-icon-min active"></i>
                 </button>
                 <input class="js-geo-city-search" type="search" name="text" placeholder="Найти город..."/>
